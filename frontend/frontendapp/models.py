7f09305e2bffabf1765241d0abf1ea34688ec3c1
Neil <ns532@cam.ac.uk>
2013-02-09 01:48:39 +0000

    New app for model/controller testing
    Just creates an extra app within the project that can hold the models
    and controllers. Eventually should be merged with the templates for
    full working MVC.

---
+++ frontend/frontendapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Artist(models.Model):
+	name = models.CharField(max_length=200)
+	fund_score = models.IntegerField('Fundamental Score')
+	market_score = models.IntegerField('Market Score')
+
+	def __unicode__(self):
+		return self.name
