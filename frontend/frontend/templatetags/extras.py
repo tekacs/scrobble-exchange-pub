16220cc3803ccdf6da0df1823258f00d839d91aa
Joe Bateson <jdb75@cam.ac.uk>
2013-02-11 23:18:16 +0000

    Update home template to tie in with API objects
    Introduce image fetching template filter to deal with missing image sizes. Change template to accommodate API objects.

---
+++ frontend/frontend/templatetags/extras.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='get_image')
+def get_image(images, size_name):
+    mega = ('mega', 'extralarge', 'large', 'medium', 'small')
+    extralarge = ('extralarge', 'large', 'medium', 'small')
+    large = ('large', 'medium', 'small')
+    medium = ('medium', 'small')
+    small = ('small',)
+    generic = 'http://some-url/generic.png'
+   
+    sizes = locals()[size_name]
+    for s in sizes:
+        if s in images:
+            return images[s]
+    return generic
