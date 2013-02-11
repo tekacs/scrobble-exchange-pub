4341ed9f2bba9f72aac7c3ecd9fda164974e8fe3
Neil <ns532@cam.ac.uk>
2013-02-11 21:39:39 +0000

    Clean up views
    Use shortcut methods for views. Broken build because the API is not yet
    integrated.

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -1,22 +1,16 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-    t = loader.get_template('index.html')
-    c = RequestContext(request, {})
-    return HttpResponse(t.render(c))
+    return render_to_response('index.html',{})
 
 def artists(request):
-    t = loader.get_template('artists.html')
-    c = RequestContext(request, {})
-    return HttpResponse(t.render(c))
+    artists = lastfm.chart.get_top_artists()
+    return render_to_response('artists.html', {'artistlist': artists})
 
 def charts(request):
-    t = loader.get_template('charts.html')
-    c = RequestContext(request, {})
-    return HttpResponse(t.render(c))
+    return render_to_response('charts.html',{})
 
 def artist_single(request, artist):
-    t = loader.get_template('artist_single.html')
-    c = RequestContext(request, {'name': artist})
-    return HttpResponse(t.render(c))
+    return render_to_response('artist_single.html', {'name': artist})
