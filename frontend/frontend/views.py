0a9ecb4cb7ac3edd4d2ff83fd09f0fe5d9998bd8
Neil <ns532@cam.ac.uk>
2013-02-12 00:18:14 +0000

    Unbreak the build

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -2,9 +2,12 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+#import se_api
+
 def home(request):
-    user_data = __portfolio(request.session['user'])
-    return render_to_response('index.html',{user_data = user_data})
+    # user_data = __portfolio(request.session['user'])
+    user_data = {}
+    return render_to_response('index.html',{'user_data': user_data})
 
 def artists(request):
     artists = se_api.lastfm.chart.get_top_artists()
@@ -16,37 +19,36 @@ def charts(request):
 def artist_single(request, artist):
     return render_to_response('artist_single.html', {'name': artist})
 
-
-
 ############ Buy/Sell ############
 
 def sell(request, artist=None, artist_id=None, price=None):
     if (request.GET or (artist==None and artist_id==None)):
         marketprice = se_api.get_sell_price(artist)
-        return render_to_response('sell.html', {marketprice = marketprice})
+        return render_to_response('sell.html', {'marketprice': marketprice})
     elif (request.POST):
+        pass
         #TODO: Authenticate
-        se_api.create_sell_orders(server=server, 
-            user=session['user'], price=price)
+        # if 'user' in request.session:
+        #     se_api.create_sell_orders(server=server, 
+        #         user=request.session['user'], price=price)
 
 def buy_search(request):
     #TODO
     pass
 
-def buy(request, artist=None, artist_id=None, price):
+def buy(request, artist=None, artist_id=None, price=None):
     #TODO
     pass
-
 
 ############ Helper Functions ############
 
 def __portfolio(user=None):
-    if (user!=None)
-    user_data = se_api.get_user_data(server=server, user=request.session['user'])
-    user_data.current_worth = sum(se_api.getArtistData(x).stockvalue
-                        for x in user_data.curstocks)
+    if user is not None:
+        user_data = se_api.get_user_data(server=server, user=user)
+        user_data.current_worth = sum(se_api.getArtistData(x).stockvalue
+                            for x in user_data.curstocks)
     return user_data
 
 def __authenticated(request):
-    if request.session['userapitoken'] == None
+    #if request.session['userapitoken'] is not None:
     pass
