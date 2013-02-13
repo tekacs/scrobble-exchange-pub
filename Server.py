d9b2eb2173029d1f8bd83921897514f47187218c
fiwl <vm305@cam.ac.uk>
2013-02-13 01:25:35 +0000

    Intermidate server code

--- Server.py
+++ Server.py
@@ -3,8 +3,6 @@
 import sys
 sys.path.append('gen-py')
 
-import config
-
 from se_api import ScrobbleExchange
 from se_api.ttypes import *
 from se_api.constants import *
@@ -15,101 +13,199 @@ from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
 #import datm
+#import config
+
+#lastfm_config = {
+    #'api_key': config.lastfmcred['api_key'],
+    #'secret' : config.lastfmcred['secret']
+#}
+
+#db_args = {
+    #'url': 'sqlite:///:memory:'
+#}
 
 class SEHandler(object):
-    #config = datm.DATMConfig(api_key=api_key, db_url='postgres://url')
+    #_config = datm.DATMConfig(lastfm=lastfm_config, db_args=db_args)
     
-    def login(self, token):
+    def apikey(self):
         """
-        If successful, returns the user token. If not, returns an
-        AuthException. AccountException is returned if account data is
-        incorrect or doesn't exist, and should be handled appropriately
+        Returns the SE API key for sending to last.fm
+        """
+        #return config.lastfmcred['apikey']
+        pass
+
+    def login(self, username, token):
+        """
+        If successful, returns the user session token.
 
         Parameters:
+        - username
         - token
         """
+        #with datm.DATMSession(_config):
+            #try:
+                #user = datm.User(self._config, username)
+                #stoken = user.getSession(self._config, token)
+        
+                #return stoken
+            #except InvalidAuthorisationException:
+                #l = LoginException()
+                #l.code = LoginCode.AUTH
+                #l.message = 'User not authenticated'
+                #raise l
         pass
 
-    def getArtist(self, artist):
+    def getArtist(self, artist, user, token):
         """
-        Returns basic artist info. Artist string can be either the name, or the
-        musicbrainz ID
+        Returns basic artist info. If either the artist or the mbid is unknown,
+        then the empty string should be sent. User will add the `ownedby'
+        property to artist
+
+        Parameters:
+        - artist
+        - user
+        - token
+        """
+        #with datm.DATMSession(_config):
+            #if not artist.mbid:
+                #a = datm.Artist(self._config, mbid = artist.mbid, user=user)
+            #else if not artist.name:
+                #a = datm.Artist(self._config, name = artist.name, user = user)
+            #else:
+                #s = SearchException()
+                #s.code = SearchCode.ARG
+                #s.message = 'Incorrect artist data'
+                #raise s
+                
+            #u = datm.User(self._config, user.name, token)
+            
+            #ret = Artist()
+            #ret.mbid = a.mbid
+            #ret.name = a.name
+            #ret.imgurls = a.images
+            #ret.ownedby = u.owns(a)
+        
+            #return ret
+        pass
+
+    def getLightArtist(self, artist, user):
+        """
+        Returns only MBID and name. If either artist or mbid are unknown, then
+        the empty string should be sent
+
+        Parameters:
+        - artist
+        - user
+        """
+        #with datm.DATMSession(_config):
+            #if not artist.mbid:
+                #a = datm.Artist(self._config, mbid = artist.mbid, user=user)
+            #else if not artist.name:
+                #a = datm.Artist(self._config, name = artist.name, user = user)
+            #else:
+                #s = SearchException()
+                #s.code = SearchCode.ARG
+                #s.message = 'Incorrect artist data'
+                #raise s
+            
+            #ret = Artist()
+            #ret.mbid = a.mbid
+            #ret.name = a.name
+        
+            #return ret
+        pass
+
+    def getArtistHistory(self, artist):
+        """
+        Returns a list of tuples of the price of the artist over time. For new
+        artists the empty list is returned.
 
         Parameters:
         - artist
         """
-   
-        #if not artist.mbid:
-            #a = datm.Artist(self.config, mbid = artist.mbid)
-        #else if not artist.name:
-            #a = datm.Artist(self.config, name = artist.name)
-        #else:
-            #raise SearchException("Incorrect artist data")
-        
-        #ret = Artist()
-        #ret.mbid = a.mbid
-        #ret.name = a.name
-        #ret.imgurls = a.images
-        
-        #return ret
         pass
 
-    def getLightArtist(self, artist):
+    def getArtistSE(self, artist, user, token):
         """
-        Returns only MusicBrainz ID and name
+        Returns the data from our db. If the artist isn't there, the data gets
+        on-demand pulled. If either artist or mbid are unknown, then the empty
+        string should be sent. User will add the `ownedby' property to artist
 
         Parameters:
         - artist
+        - user
+        - token
         """
-        #if not artist.mbid:
-            #a = datm.Artist(self.config, mbid = artist.mbid)
-        #else if not artist.name:
-            #a = datm.Artist(self.config, name = artist.name)
-        #else:
-            #raise SearchException("Incorrect artist data")
-        
-        #ret = Artist()
-        #ret.mbid = a.mbid
-        #ret.name = a.name
-        
-        #return ret
+        #with datm.DATMSession(_config):
+            #if not artist.mbid:
+                #a = datm.Artist(self._config, mbid = artist.mbid, 
+                    #user=user)
+            #else if not artist.name:
+                #a = datm.Artist(self._config, name = artist.name, user = 
+                    #user)
+            #else:
+                #s = SearchException()
+                #s.code = SearchCode.ARG
+                #s.message = 'Incorrect artist data'
+                #raise s
+            
+            #u = datm.User(self._config, user.name, token)
+            
+            #r = Artist()
+            #r.mbid = a.mbid
+            #r.name = a.name
+            #r.imgurls = a.images
+            #r.ownedby = u.owns(a)
+            
+            #ret = ArtistSE()
+            #ret.artist = r
+            #ret.price = a.price
+            #ret.num_remaining = a.no_remaining
+            
+            #return ret
         pass
 
-    def getArtistSE(self, artist):
+    def getArtistLFM(self, artist, user, token):
         """
-        Returns the data from our db for the artist.
-        Assumes that if artist isn't in the DB, then it gets pulled in
-        on-demand and so will always return some data.
-        Artist string can be either the name, or the musicbrainz ID
+        Returns the artist info from last.fm for the artist. If either artist
+        or mbid are unknown, then the empty string should be sent. User will add
+        the `ownedby' property to the artist
 
         Parameters:
         - artist
+        - user
+        - token
         """
-        #if not artist.mbid:
-            #a = datm.Artist(self.config, mbid = artist.mbid)
-        #else if not artist.name:
-            #a = datm.Artist(self.config, name = artist.name)
-        #else:
-            #raise SearchException("Incorrect artist data")
+        #with datm.DATMSession(_config):
+            #if not artist.mbid:
+                #a = datm.Artist(self._config, mbid = artist.mbid, 
+                    #user=user)
+            #else if not artist.name:
+                #a = datm.Artist(self._config, name = artist.name, user = 
+                    #user)
+            #else:
+                #s = SearchException()
+                #s.code = SearchCode.ARG
+                #s.message = 'Incorrect artist data'
+                #raise s
+            
+            #u = datm.User(self._config, user.name, token)
+            
+            #r = Artist()
+            #r.mbid = a.mbid
+            #r.name = a.name
+            #r.imgurls = a.images
+            #r.ownedby = u.owns(a)
+            
+            #ret = ArtistLFM()
+            #ret.artist = a.streamable
+            #ret.listeners = a.listeners
+            #ret.plays = a.plays
+            #ret.similar = a.similar
+            #ret.bio = a.bio
+            
+            #return ret
         
-        #ret = Artist()
-        #ret.mbid = a.mbid
-        #ret.name = a.name
-        #ret.imgurls = a.images
-        #ret.price = a.price
-        #ret.num_remaining = a.no_remaining
-        
-        #return ret
-        pass
-
-    def getArtistLFM(self, artist):
-        """
-        Returns the contextual artist info from last.fm for the artist
-        Artist string can be either the name or the musicbrainz ID
-
-        Parameters:
-        - artist
-        """
         pass
 
     def searchArtist(self, text):
@@ -124,9 +220,9 @@ class SEHandler(object):
 
     def getTopArtists(self, n, tag):
         """
-        Returns a list of the n top artists by decreasing value. By default, tag
-        should have a value of '' and only be used if you want to limit the
-        top lists to a certain tag.
+        Returns a list of the n top artists by decreasing value. By default,
+        tag should be the empty string, and only used if you want specific tag
+        access.
 
         Parameters:
         - n
@@ -143,22 +239,42 @@ class SEHandler(object):
         """
         pass
 
-    def getUserData(self, user):
+    def getRecentTrades(self, n):
         """
-        Returns extended user data for the current user
+        Returns a list of the n most recent trades
 
         Parameters:
-        - user
+        - n
         """
         pass
 
-    def getUserInfo(self, user):
+    def getRecentArtistTrades(self, n):
+        """
+        Returns a list of the n most recent trades of this artist
+
+        Parameters:
+        - n
+        """
+        pass
+
+    def getUserData(self, user, token):
+        """
+        Returns extended user data for the current user. Requires the session
+        token for validation
+
+        Parameters:
+        - user
+        - token
+        """
+        pass
+
+    def getUserInfo(self, userstr):
         """
         Returns extended user data for the user in the string. Mostly, used for
         profile pages
 
         Parameters:
-        - user
+        - userstr
         """
         pass
 
@@ -172,19 +288,21 @@ class SEHandler(object):
         """
         pass
 
-    def getNearUsers(self, user):
+    def getNearUsers(self, user, token):
         """
         Returns a list of 10 users with 4 above and 5 below in the leaderboard
-        compared to the user provided
+        compared to the user provided, including the user's position. Requires
+        the session token for validation
 
         Parameters:
         - user
+        - token
         """
         pass
 
-    def getTransaction(self, artist):
+    def getGuarantee(self, artist):
         """
-        Returns the guarantee token to the front end
+        Returns the guarantee token (elephant) to the front end
 
         Parameters:
         - artist
@@ -194,9 +312,7 @@ class SEHandler(object):
     def buyArtist(self, transaction, user):
         """
         Buys artist for user, and returns the new value of that stock in the
-        game. Throws a transaction exception if something goes wrong while
-        buying or the user can't afford to buy the artist.
-        Throws user exception if the user already owns the stock
+        game.
 
         Parameters:
         - transaction
@@ -206,15 +322,14 @@ class SEHandler(object):
 
     def sellArtist(self, transaction, user):
         """
-        Sells artist for user, and returns the new value of that artist. User
-        exception is thrown if the user isn't allowed to sell or doesn't own
-        that artist
+        Sells artist for user, and returns the new value of that artist.
 
         Parameters:
         - transaction
         - user
         """
         pass
+
 
 processor = ScrobbleExchange.Processor(SEHandler())
 transport = TSocket.TServerSocket(port=9090)
