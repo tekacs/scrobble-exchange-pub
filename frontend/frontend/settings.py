3191987c7de57b9006394c28d0a897963dbf82e6
Amar Sood (tekacs) <mail@tekacs.com>
2013-02-13 04:11:00 +0000

    Changed database to sqlite3 for dev

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'scrobblefront',                      # Or path to database file if using sqlite3.
         'USER': 'charlie',                      # Not used with sqlite3.
         'PASSWORD': 'myritylv',                  # Not used with sqlite3.
