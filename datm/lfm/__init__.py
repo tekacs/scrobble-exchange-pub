5c015e4c4b6a0b81ac34dc46830ae3f8bd3aaab0
Amar Sood (tekacs) <mail@tekacs.com>
2013-02-20 01:34:13 +0000

    Added lfm in a still-inconsistent, but progressed state.
    Mainly so you can see what I am up to and why it has yet to be finished. :P

--- datm/lfm/__init__.py
+++ datm/lfm/__init__.py
@@ -3,10 +3,8 @@ __copyright__ = "Copyright (C) 2013 - \infty Amar Sood"
 __license__ = "apache2"
 __email__ = 'mail@amarsood.com'
 
-raise DeprecationWarning('This code is near-complete, but is _not_ in use.')
-
-from api_base import Server, APIResponse
-from artist import Artist
-
-
-__all__ = ['Server', 'APIResponse']
+#from api_base import Server, APIResponse
+#from artist import Artist
+#
+#
+#__all__ = ['Server', 'APIResponse']
--- datm/lfm/api_base.py
+++ datm/lfm/api_base.py
@@ -1,34 +1,39 @@
 import warnings
 from hashlib import md5 as _md5
+from functools import wraps as _wraps
 
 import requests
 import types
 
 __all__ = ['Server']
 
-class Server(object):
+class APIClient(object):
     _base_endpoint = 'http://ws.audioscrobbler.com/2.0/'
     _base_params = {'format': 'json'}
 
-    def __init__(self, api_key, user=None, session_key=None):
+    def __init__(self, api_key, api_secret=None):
+        """The basis for a last.fm API server connection:
+            api_key (required) :: needed to perform any operations on the API.
+            api_secret :: required to perform any authenticated operations on the API
+        """
         self._base_params.update(api_key=api_key)
-#        [self._base_params.update({v: locals()[v]}) for v in ('user', 'session_key') if not v is None]
-        if not user is None:
-            self._base_params['user'] = user
-        if not session_key is None:
-            self._base_params['sk'] = session_key
+        if not api_secret is None:
+            self._base_params['api_secret'] = api_secret
 
     @property
     def api_key(self):
         return self._base_params['api_key']
 
     @property
-    def user(self):
-        return self._base_params['user']
+    def api_secret(self):
+        try:
+            return self._base_params['api_secret']
+        except KeyError:
+            raise AttributeError('api_secret')
 
-    @property
-    def session_key(self):
-        return self._base_params['sk']
+    @api_secret.setter
+    def api_secret(self, value):
+        self._base_params['api_secret'] = value
 
     def _sign(self, params):
         sig = ''
@@ -46,14 +51,18 @@ class Server(object):
         self._sign(params)
         return requests.post(self._base_endpoint, params=params).json()
 
+class AuthClient(object):
+    def __init__(self, user, session_key=None):
+        self.user = user
+        self.session_key = session_key
+
 class APIMeta(type):
     """Define all possible methods on the class at creation-time."""
     def __new__(mcls, name, bases, attrs):
         cls = type.__new__(mcls, name, bases, attrs)
         local_getattr = types.UnboundMethodType(cls, mcls._get_attr)
         for method in attrs['_methods']:
-            attrs[mcls._camelcase_to_underscores(method)] = local_getattr
-        attrs['_name'] = name
+            setattr(cls, mcls._camelcase_to_underscores(method), local_getattr)
         return cls
 
     @staticmethod
@@ -64,7 +73,7 @@ class APIMeta(type):
     def _get_attr(self, method, type='read', **kwargs):
         def _default_transformer(self, json_dict):
             json_dict['body_text'] = json_dict.pop('#text')
-            return type('anon', (APIResponse,), json_dict)
+            return type(method, (APIResponse,), json_dict)
         json_dict = getattr(self._server, type)(namespace=self._name, **kwargs)
         trans = _default_transformer
         if method in self._transformers: # robots in disguise!
@@ -75,13 +84,105 @@ class APIObject(object):
     pass
 
 def transformer(cls, type='read'):
-    def dec(f):
+    def decorator(f):
         if not f.func_name in cls._methods:
             warnings.warn('Tried to define transformer ' + f.func_name + ' - not in _methods!')
         cls._transformers[f.func_name] = f
 
         return f
-    return dec
+    return decorator
 
 class APIResponse(object):
     pass
+
+# And now begins the magic...
+# Beware of Python(ism)s
+
+class frozendict(dict):
+    '''A frozen dictionary class that can be hashed and indeed has hashing
+        behaviour similar to the native behaviour (in PyPy, at least :P).
+
+        Please note that this dict isn't _actually_ frozen. Doing so would
+        require effort upon instantiation (you can't use metaclasses to
+        customise builtin class creation (given that they're actually written
+        and instantiated in C) afaik) and since the very point of this class
+        is performance, I don't bother with that.
+
+        The name is more of a hint to warn the user not to mess with it after
+        creation, as doing so would invalidate the hash, which will not be
+        updated appropriately.
+
+        It would be quite trivially possible to extend this to include hash
+        recalculation but again: *performance*.'''
+    __slots__ = ('_hash',)
+    def __hash__(self):
+        ret = getattr(self, '_hash', None)
+        if ret is None:
+            ret = self._hash = hash(frozenset(self.iteritems()))
+        return ret
+
+def memoise(f):
+    f.memo = memo = {}
+    @_wraps(f)
+    def wrapper(*args, **kwargs):
+        uniq = (args, frozendict(kwargs))
+        ret = memo.get(uniq)
+        if ret is None:
+            ret = memo[uniq] = f(*args, **kwargs)
+        return ret
+    return wrapper
+
+def recursive_wrap(attr):
+    def decorator(f):
+        @_wraps(f)
+        def wrapper(self, *args, **kwargs):
+            if getattr(self, attr, False):
+                return type(self)(f(self, *args, **kwargs), recursive=True)
+            else:
+                return f(self, *args, **kwargs)
+        return wrapper
+    return decorator
+
+class dotmagic(object):
+    """Django templates do this for dot lookup.
+
+    I thought it was cool, so I implemented something like it.
+
+    Essentially makes attributes, 'no-argument' method calls and dictionary
+    accesses all possible homogeneously via attribute access or dictionary
+    access.
+
+    The order of lookup is dict > callable, 'no-arg' attr > straight attr.
+    """
+
+    def __init__(self, target, recursive=False):
+        self.__target = target
+        self.__recursive = recursive
+
+    @recursive_wrap('_dotmagic__recursive')
+    def __getval(self, name):
+        target = self.__target
+
+        try:
+            return target[name]
+        except (KeyError, TypeError, IndexError):
+            pass
+
+        try:
+            return getattr(target, name)()
+        except (AttributeError, TypeError):
+            pass
+
+        try:
+            return getattr(target, name)
+        except AttributeError:
+            pass
+
+        raise AttributeError(name)
+
+    __getattr__ = __getval
+    __getitem__ = __getval
+
+    def __call__(self):
+        """Gets the target value without recursively wrapping."""
+        return self.__target
