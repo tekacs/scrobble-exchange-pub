c166ca4dd9bd14bcdb09a7327b2cedab1ce1085c
Joe Bateson <jdb75@cam.ac.uk>
2013-02-25 22:27:51 +0000

    Add basic typeahead function to artists search bar

--- frontend/frontend/static/js/general.js
+++ frontend/frontend/static/js/general.js
@@ -1,2 +1,11 @@
 // Define global object to contain all variables/methods
 var SE = SE || {};
+
+jQuery(document).ready(function($) {
+    $('.typeahead-search').typeahead({
+        name: 'artists',
+        remote: '../static/js/test_typeahead.json?%QUERY',
+        local: ['coldplay_local', 'coldplayerz_local', 'nickelback_local', 'daft punk_local'],
+        limit: 10
+    });
+});
