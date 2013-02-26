e04965557844ceb3fc4275018051d80bc16eb26d
Amar Sood (tekacs) <mail@tekacs.com>
2013-02-26 00:19:12 +0000

    Made lfm fully import-able.

--- datm/lfm/__init__.py
+++ datm/lfm/__init__.py
@@ -5,10 +5,23 @@ __copyright__ = "Copyright (C) 2013 - \infty Amar Sood"
 __license__ = "apache2"
 __email__ = 'mail@amarsood.com'
 
-from .lastfm import RequestBuilder
+from .lastfm import RequestBuilder, TransientError, AuthenticationError,\
+    DataError, ProgrammingError, ServiceError
 from .artist import Artist
 from .user import User
 from .auth import Auth
 from .chart import Chart
 
-__all__ = ['RequestBuilder', 'Artist', 'User', 'Auth', 'Chart']
+__all__ = [
+    'RequestBuilder',
+    'Artist',
+    'User',
+    'Auth',
+    'Chart',
+
+    'TransientError',
+    'AuthenticationError',
+    'DataError',
+    'ProgrammingError',
+    'ServiceError'
+]
