9f4f55aa1e105fd34f5b0f796af2ce194a89930f
Neil <ns532@cam.ac.uk>
2013-02-27 00:52:42 +0000

    Bugfix search to treat 'lucky' param as string not bool. Also return query string to search results page.

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -286,12 +286,12 @@ def search(request):
 
     query = request.GET.get('q')
     results = client.searchArtist(query)
-    if (request.GET.get('lucky', 'false') == true):
+    if (request.GET.get('lucky', 'false') == 'true'):
         #TODO: Pass ID instead of name once artist_single can handle it
         return redirect('artist_single', artistname=results[0].name)
     else:
         return render_to_response('search_results.html', {
-            'form': form, 'results': results}, context_instance=RequestContext(request))
+            'query':query, 'results': results}, context_instance=RequestContext(request))
 
 
 ############ Buy/Sell ############
