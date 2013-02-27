e1d3b401c941e43811cd94cae650b791aad59937
fiwl <vm305@cam.ac.uk>
2013-02-27 12:03:17 +0000

    Change views.py to pull from remote API

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -11,64 +11,33 @@ from utils import json_response
 from se_api import ttypes
 
 import random
+from urllib import quote
 
 RESULTS_PER_PAGE = 9;
-client =  settings.CLIENT
+client = settings.CLIENT
 #TODO: Handle exceptions with the client
 
 def home(request):
-    # Dummy artist data
+    #Done with the API
+    api_user_data = client.getUserData('fiwl')
+    authorized_user = ttypes.AuthUser(name = ttypes.User(name = 'fiwl'), 
+        session_key = 'thekeyyougot')
+    
     user_data = models.UserData()
-    user_data.stocks = [
-        {
-            'artist': {
-                'name': 'Coldplay',
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/500\/75646980\/Coldplay+PNG.png',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/75646980.png'
-                    # in real data, get all available image urls
-                }
-            },
-            'price': 2500
-        },
-        {
-            'artist': {
-                'name': 'Daft Punk',
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/500\/4183432\/Daft+Punk+daftpunk_1.jpg',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/4183432.jpg'
-                    # in real data, get all available image urls
-                }
-            },
-            'price': 2200
-        },
-        {
-            'artist': {
-                'name': 'Gorillaz',
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/_\/411274\/Gorillaz.jpg',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/411274.jpg'
-                    # in real data, get all available image urls
-                }
-            },
-            'price': 2300
-        },
-        {
-            'artist': {
-                'name': 'A random band with a missing image and a long name! (and punctuation)',
-                'imgurls': {
-                    # in real data, get all available image urls
-                }
-            },
-            'price': 100
-        }]
-    user_data.user = {'money': 140512, 'points': 242}
-    user_data.portfolio_worth = sum(artist_SE['price'] for artist_SE in user_data.stocks)
+    user_data.stocks = []
+    
+    for artistse in api_user_data.stocks:
+        a = {'name': artistse.artist.name, 'imgurls': 
+                                                    artistse.artist.imgurls}
+        user_data.stocks.append({'artist': a, 'price': artistse.price})
+        
+    user_data.user = {'money': client.getUserMoney(authorized_user).money, 
+                    'points': api_user_data.user.points}
+    user_data.portfolio_worth = sum(artist.price for artist in 
+                                                    api_user_data.stocks)
 
-    return render_to_response(
-        'index.html',
-        {'user_data': user_data},
-        context_instance=RequestContext(request)
+    return render_to_response('index.html',
+        {'user_data': user_data}, context_instance=RequestContext(request)
         )
 
 def user_profile(request, username):
@@ -79,132 +48,67 @@ def user_profile(request, username):
 def leaderboards(request):
     # leaderboard = client.getNearUsers(request.user)
     userleaderboard = {}
-    return render_to_response('leaderboards.html',{'leaderboard': userleaderboard}, context_instance=RequestContext(request))
+    return render_to_response('leaderboards.html',{'leaderboard': 
+           userleaderboard}, context_instance=RequestContext(request))
 
 '''See http://localhost:8000/leaderboards/get/?league_id=1&time_range=3 for example'''
 @json_response
 def get_leaderboard(request):
+    
     league_id = request.GET.get('league_id','default_league')
     time_range = request.GET.get('time_range', 'default_time_range')
-    # leaderboard = client.getTopUsers(RESULTS_PER_PAGE, league_id, time_range)
-
-    # Data must be in this format to work with jQuery datatables library, need to craft json into this format
+    
+    # none of the below values make any difference with dummy data 
+    board = client.getTopUsers(n=3, league = ttypes.League(name=league_id), 
+    trange = 70)
+    
+    aadata = []
+    for i in range(len(board.users)):
+        aa = {
+            '0': str(i+1),
+            '1': '<a hred = "#"><img src=\"http://lorempixel.com/48/48/\">'\
+            +board.users[i].name+'</a>',
+            '2': str(board.users[i].points),
+            'DT_RowClass': 'place-'+str(i+1)
+        }
+        
+        aadata.append(aa)
+        
     leaderboard = {
-        "sEcho": 1,
-        "iTotalRecords": "50",
-        "iTotalDisplayRecords": "50",
-        "aaData":[
-            {
-                "0": "1",
-                "1": "<a href=\"#\"><img src=\"http://lorempixel.com/48/48/\"> neil-s</a>",
-                "2": "70",
-                "DT_RowClass": "place-1"
-            },
-            {
-                "0": "2",
-                "1": "<a href=\"#\"><img src=\"http://lorempixel.com/48/48/\"> joebateson</a>",
-                "2": "40",
-                "DT_RowClass": "place-2 me"
-            },
-            {
-                "0": "3",
-                "1": "<a href=\"#\"><img src=\"http://lorempixel.com/48/48/\"> rand</a>",
-                "2": "20",
-                "DT_RowClass": "place-3"
-            }
-        ]
+        'sEcho': 1,
+        'iTotalRecords': '50',
+        'iTotalDisplayRecords': '50',
+        'aaData': aadata
     }
+    
     return leaderboard
 
 
 ############ Artist stuff ############
 def artists(request):
-    # artistlist = se_api.lastfm.chart.get_top_artists()
-    artist1 = models.Artist()
-    artist1.name = 'Iron Maiden'
-    artist2 = models.Artist()
-    artist2.name = 'Foo Fighters'
+    authorized_user = ttypes.AuthUser(name = ttypes.User(name = 'fiwl'), 
+                                                session_key = 'thekeyyougot')
+    artist1 = ttypes.Artist(name = 'Iron Maiden')
+    artist2 = ttypes.Artist(name = 'Foo Fighters')
     artistlist = [artist1, artist2]
 
-    top_SE_artists = [
-        {
-            'artist': {
-                'name': 'Coldplay',
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/500\/75646980\/Coldplay+PNG.png',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/75646980.png',
-                    'large': 'http://userserve-ak.last.fm/serve/126/75646980.png',
-                    'medium': 'http://userserve-ak.last.fm/serve/64/75646980.png',
-                    'small': 'http://userserve-ak.last.fm/serve/34/75646980.png'
-                }
-            },
-            'price': 2542,
-            'points': 242
-        },
-        {
-            'artist': {
-                'name': 'Flight of the Conchords',
-                'imgurls': {
-                    'mega': 'http://userserve-ak.last.fm/serve/_/22957595/Flight+of+the+Conchords+flight.jpg',
-                    'extralarge': 'http://userserve-ak.last.fm/serve/252/22957595.jpg',
-                    'large': 'http://userserve-ak.last.fm/serve/126/22957595.jpg',
-                    'medium': 'http://userserve-ak.last.fm/serve/64/22957595.jpg',
-                    'small': 'http://userserve-ak.last.fm/serve/34/22957595.jpg'
-                }
-            },
-            'price': 2401,
-            'points': 309
-        },
-        {
-            'artist': {
-                'name': 'Daft Punk',
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/500\/4183432\/Daft+Punk+daftpunk_1.jpg',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/4183432.jpg',
-                    'large': 'http://userserve-ak.last.fm/serve/126/4183432.jpg',
-                    'medium': 'http://userserve-ak.last.fm/serve/64/4183432.jpg',
-                    'small': 'http://userserve-ak.last.fm/serve/34/4183432.jpg'
-                }
-            },
-            'price': 2200,
-            'points': 420
-        },
-        {
-            'artist': {
-                'name': 'The Killers',
-                'imgurls': {
-                    'mega': 'http://userserve-ak.last.fm/serve/500/82785611/The+Killers+tumblr_1280.png',
-                    'extralarge': 'http://userserve-ak.last.fm/serve/252/82785611.png',
-                    'large': 'http://userserve-ak.last.fm/serve/126/82785611.png',
-                    'medium': 'http://userserve-ak.last.fm/serve/64/82785611.png',
-                    'small': 'http://userserve-ak.last.fm/serve/34/82785611.png'
-                }
-            },
-            'price': 1920,
-            'points': 523
-        },
-        {
-            'artist': {
-                'name': 'Hans Zimmer',
-                'imgurls': {
-                    'mega': 'http://userserve-ak.last.fm/serve/500/73701504/Hans+Zimmer+hz4.png',
-                    'extralarge': 'http://userserve-ak.last.fm/serve/252/73701504.png',
-                    'large': 'http://userserve-ak.last.fm/serve/126/73701504.png',
-                    'medium': 'http://userserve-ak.last.fm/serve/64/73701504.png',
-                    'small': 'http://userserve-ak.last.fm/serve/34/73701504.png'
-                }
-            },
-            'price': 1605,
-            'points': 688
-        },
-    ]
-
-    popular_LFM_artists = list(top_SE_artists)
-    random.shuffle(popular_LFM_artists)
-
+    api_top_artists = client.getSETop(5, 5)
+    top_SE_artists = []
+    for i in api_top_artists:
+        a = client.getArtistSE(i, authorized_user)
+        artist = {'name': i.name, 'imgurls': i.imgurls}
+        top_SE_artists.append({'artist':artist, 'price': a.price, 'points': 
+        a.points})
+    
+    rising_SE_artists = list(top_SE_artists)
+    random.shuffle(rising_SE_artists)
+    
     recommended_artists = list(top_SE_artists)
     random.shuffle(recommended_artists)
-
+    
+    popular_LFM_artists = list(top_SE_artists)
+    random.shuffle(popular_LFM_artists)
+    
     recently_traded_artists = list(top_SE_artists)
     random.shuffle(recently_traded_artists)
 
@@ -221,69 +125,22 @@ def artist_single(request, artistname):
     #TODO: Change artist name mechanism, look at http://stackoverflow.com/a/837835/181284
     if (artistname == ''):
         return redirect('artists')
-    example_bio = """Coldplay is a British <a
-    href="http://www.last.fm/tag/alternative%20rock"
-    class="bbcode_tag" rel="tag">alternative rock</a> band, formed
-    in London, United Kingdom in 1997. The band comprises vocalist
-    and pianist <a href="http://www.last.fm/music/Chris+Martin"
-    class="bbcode_artist">Chris Martin</a>, lead guitarist <a
-    href="http://www.last.fm/music/Jonny+Buckland"
-    class="bbcode_artist">Jonny Buckland</a>, bassist <a
-    href="http://www.last.fm/music/Guy+Berryman"
-    class="bbcode_artist">Guy Berryman</a>, and drummer <a
-    href="http://www.last.fm/music/Will+Champion"
-    class="bbcode_artist">Will Champion</a>. Having released four
-    successful albums, (all of which debuted at #1 on the UK album
-    chart) Coldplay have also achieved great success with their
-    singles, such as <a title="Coldplay &ndash; Yellow"
-    href="http://www.last.fm/music/Coldplay/_/Yellow"
-    class="bbcode_track">Yellow</a>, <a title="Coldplay &ndash;
-    Speed of Sound"
-    href="http://www.last.fm/music/Coldplay/_/Speed+of+Sound"
-    class="bbcode_track" >Speed of Sound</a>, the Grammy-winning <a
-    title="Coldplay &ndash; Clocks"
-    href="http://www.last.fm/music/Coldplay/_/Clocks"
-    class="bbcode_track">Clocks</a> and the US and UK #1 single <a
-    title="Coldplay &ndash; Viva la Vida"
-    href="http://www.last.fm/music/Coldplay/_/Viva+la+Vida"
-    class="bbcode_track">Viva la Vida</a>. Frontman Chris Martin
-    credits 1980s Norwegian pop band <a
-    href="http://www.last.fm/music/a-ha"
-    class="bbcode_artist">a-ha</a> for inspiring him to form his
-    own band."""
-    artist = type('Artist', (), {
-        'name': artistname,
-        'bio': {
-            'summary': example_bio
-        },
-        'similar': [
-            {
-                'name': 'Daft Punk',
-                'current_price': 2200,
-                'imgurls': {
-                    'mega': 'http:\/\/userserve-ak.last.fm\/serve\/500\/4183432\/Daft+Punk+daftpunk_1.jpg',
-                    'extralarge': 'http:\/\/userserve-ak.last.fm\/serve\/252\/4183432.jpg'
-                    # in real data, get all available image urls
-                }
-            },
-            {
-                'name': 'A random band with a missing image and a long name! (and punctuation)',
-                'current_price': 100,
-                'imgurls': {
-                    # in real data, get all available image urls
-                }
-            }
-        ]
-    })();
-    artist_SE = type('ArtistSE', (), {
-        'artist': artist,
-        'ownedby': artist.name != 'Nickelback',
-        'price': 2300,
-        'points': 103,
-        'dividends': 41
-        })();
+    authorized_user = ttypes.AuthUser(name = ttypes.User(name = 'fiwl'), 
+                                                session_key = 'thekeyyougot')
+    artist_basic = client.getArtist(ttypes.Artist(mbid = '', name = 
+                    artistname))
+    artist_se = client.getArtistSE(artist_basic, authorized_user)
+    artist_lfm = client.getArtistLFM(artist_basic, authorized_user)
+    
+    artist = {'name':artist_basic.name, 
+            'bio':{'summary': artist_lfm.bio.summary}, 'similar': 
+            artist_lfm.similar}
+    
+    artist_sse = {'artist': artist, 'ownedby': artist_se.ownedby, 
+                'price': artist_se.price, 'points': 103, 'dividends': 41}
 
-    return render_to_response('artist_single.html', {'artist_SE': artist_SE}, context_instance=RequestContext(request))
+    return render_to_response('artist_single.html', {
+        'artist_SE':artist_sse}, context_instance=RequestContext(request))
 
 ''' Sample URL: http://127.0.0.1:8000/artist/history/?artist_id=0383dadf-2a4e-4d10-a46a-e9e041da8eb3&days=2'''
 @json_response
@@ -312,7 +169,21 @@ def auto_complete(request):
     #TODO: Change format of data
     partial_text = request.GET.get('q','')
     results = client.searchArtist(partial_text, RESULTS_PER_PAGE, 1)
-    return results
+    
+    auto = []
+    for a in results:
+        adict = {
+            'value': a.name,
+            'url': quote('/artist/'+a.name),
+        }
+        try:
+            adict['img'] = a.imgurls['mega']
+        except KeyError:
+            pass
+        
+        auto.append(adict)
+        
+    return auto
 
 ''' Sample URL: http://localhost:8000/search/?q=blah . See https://docs.djangoproject.com/en/dev/topics/forms/ '''
 def search(request):
@@ -356,6 +227,8 @@ def price(request, artist_id=None):
     authuser =  __authuser(request)
     # artist_SE = client.getArtistSE(artist = artist, user = authuser)
     # TODO: Ask Victor why this may be throwing exceptions
+    # Throws errors because the test backend currently only looks stuff up by 
+    # artist name
     artist_SE = {
         'artist': {
             'name': 'Coldplay',
