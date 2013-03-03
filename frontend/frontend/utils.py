0a3d2bec6e1e7951de3c76dd38aeb972c94b5076
Amar Sood (tekacs) <mail@tekacs.com>
2013-03-03 03:57:05 +0000

    Fix json_response decorator.

--- frontend/frontend/utils.py
+++ frontend/frontend/utils.py
@@ -9,28 +9,30 @@ from thrift.transport import TSocket, TTransport
 from thrift.protocol import TBinaryProtocol
 # End of thrift
 
-def json_response(func, auth_needed = False):
+def json_response(auth_needed = False):
     """
     A decorator thats takes a view response and turns it
     into json. If a callback is added through GET or POST
     the response is JSONP.
     """
-    def decorator(request, *args, **kwargs):
-        if auth_needed and not request.user.is_authenticated():
-            return HttpResponseForbidden()
+    def decorator(func):
+        def inner(request, *args, **kwargs):
+            if auth_needed and not request.user.is_authenticated():
+                return HttpResponseForbidden()
 
-        objects = func(request, *args, **kwargs)
-        if isinstance(objects, HttpResponse):
-            return objects
-        try:
-            data = json.dumps(objects)
-            if 'callback' in request.REQUEST:
-                # a jsonp response!
-                data = '%s(%s);' % (request.REQUEST['callback'], data)
-                return HttpResponse(data, "text/javascript")
-        except:
-            data = json.dumps(str(objects))
-        return HttpResponse(data, "application/json")
+            objects = func(request, *args, **kwargs)
+            if isinstance(objects, HttpResponse):
+                return objects
+            try:
+                data = json.dumps(objects)
+                if 'callback' in request.REQUEST:
+                    # a jsonp response!
+                    data = '%s(%s);' % (request.REQUEST['callback'], data)
+                    return HttpResponse(data, "text/javascript")
+            except:
+                data = json.dumps(str(objects))
+            return HttpResponse(data, "application/json")
+        return inner
     return decorator
 
 def get_current_path(request):
