1d61d55a2465647029fb72d4c515b92f33236ef3
Amar Sood (tekacs) <mail@tekacs.com>
2013-03-04 01:52:00 +0000

    Updates to Artist.

--- datm/artist.py
+++ datm/artist.py
@@ -49,7 +49,7 @@ class Artist(DATMObject):
             pass
         try:
             return self.lastfm_info['name']
-        except (NoLastFMException, KeyError):
+        except (NoLastFMException, KeyError, AttributeError, TypeError):
             pass
 
         # In theory @require_data_source should prevent this from occurring.
@@ -57,48 +57,15 @@ class Artist(DATMObject):
 
     @memoised_property
     def mbid(self):
-        """Return the MBID corresponding to this object.
-
-        Recognising that a request for this property should never cause loops
-        is tricky at best.
-
-        - Upon entering this method, both of its approaches to
-        determining mbid ostensibly rely upon calls to this function itself!
-
-        - Both methods try to query by mbid before doing a query by name and as
-        such check this very property.
-
-        - As such, this method sets self._mbid to None before making any such
-        calls. This means that any requests for mbid whilst it is in the
-        process of being determined will evaluate directly to None. This is to
-        some extent a hack, and a thread-unsafe hack at that, but at least in
-        principle, it should never *be* that two threads are directly addressing
-        a single Artist instance.
-
-        It should be noted that if the body of this method is being called then
-        it cannot be that self._mbid was already set at the time of the method
-        call and so no existing value need be stored for restoration.
-
-        - Upon a successful return of the method, the wrapping decorator sets
-        self._mbid to the returned value and all is right in the world again.
-
-        - Upon a failed call, self._mbid is deleted.
-
-        This should all only ever be a concern if this method is called solely
-        with a name (no mbid, no pre-populated dbo or lastfm_info).
-        """
-        self._mbid = None
-
+        """Return the MBID corresponding to this object."""
         try:
             return self.lastfm_info['mbid']
-        except (NoLastFMException, KeyError):
+        except (NoLastFMException, KeyError, AttributeError, TypeError):
             pass
         try:
             return self.dbo.mbid
-        except (NoDatabaseException, NoDatabaseObjectException):
+        except (NoDatabaseException, NoDatabaseObjectException, AttributeError):
             pass
-
-        del self._mbid
 
 #       FIXME: Removed @require_data_source due to infinite recursion despite care :/
 #        In theory @require_data_source should prevent this from occurring.
@@ -124,6 +91,8 @@ class Artist(DATMObject):
     @require_db
     def create(self, price, max_available):
         """Create a DB Object corresponding to this Artist."""
+        if self.mbid == '':
+            raise ArtistNeedsMBIDException()
         self.dbo = models.Artist(
             mbid=self.mbid,
             name=self.name,
@@ -132,32 +101,26 @@ class Artist(DATMObject):
         )
         self.session.db.add(self.dbo)
 
-    @property
-    @require_db
-    def persisted(self):
-        """Check if a database object exists corresponding to this artist."""
-        try:
-            if self.dbo is not None:
-                return True
-        except NoDatabaseObjectException:
-            return False
-
     @memoised_property
     @require_lastfm
     def lastfm_info(self):
         """The lastfm get_info data corresponding to this Artist."""
-        # FIXME: Underscore hack.
-        if self._mbid is not None:
+
+        try:
+            mbid = self.mbid
             return lfm.Artist.get_info(lastfm.params(
                 self.config,
-                mbid=self._mbid
+                mbid=mbid
             ))
-        else:
-            return lfm.Artist.get_info(lastfm.params(
-                self.config,
-                artist=self._name,
-                autocorrect=1
-            ))
+        except AttributeError:
+            try:
+                return lfm.Artist.get_info(lastfm.params(
+                    self.config,
+                    artist=self.name,
+                    autocorrect=1
+                ))
+            except AttributeError:
+                raise AttributeError('lastfm_info')
 
     # Static Methods
 
@@ -233,11 +196,14 @@ class Artist(DATMObject):
 
     max_available = db.dbo_property('max_available')
     price = db.dbo_property('price')
+    last_playcount = db.dbo_property('last_playcount')
+    last_listeners = db.dbo_property('last_listeners')
+    last_closing_price = db.dbo_property('last_closing_price')
 
     @property
     @require_db
     def points(self):
-        return self.history[0].points
+        return self.history(count=1)[0].points
 
     @property
     @require_db
@@ -246,14 +212,13 @@ class Artist(DATMObject):
         return self.dbo.no_remaining
 
     @require_db
-    def history(self, after, step=None):
-        # FIXME: There has *got* to be a way to fetch FKs with a limit. :/
-        history = self.dbo.history
-        for i in history:
-            if i.date >= after:
-                yield i
-            else:
-                raise StopIteration()
+    def history(self, after=None, step=None, count=None):
+        q = db.query(self.config, models.ArtistHistory).join(models.Artist)
+        if after is not None:
+            q = q.filter_by(models.ArtistHistory.date >= after)
+        q = q.order_by(models.ArtistHistory.date.desc()).limit(count)
+
+        return q.all()
 
     @memoised_property
     @require_lastfm
@@ -267,8 +232,13 @@ class Artist(DATMObject):
 
     @property
     @require_lastfm
-    def plays(self):
+    def playcount(self):
         return int(self.lastfm_info['stats']['playcount'])
+
+    @property
+    @require_lastfm
+    def plays(self):
+        return self.playcount
 
     @property
     @require_lastfm
@@ -300,6 +270,9 @@ def partial_artist(config, partial_info):
             )
         )
     )
+
+class ArtistNeedsMBIDException(lfm.lastfm.DataError):
+    message = 'We don\'t deal in artists with no MBID!'
 
 class NoStockRemainingException(Exception):
     pass
