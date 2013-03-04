4dd6c22149a1089343080a7d510e01d3430865ac
Amar Sood (tekacs) <mail@tekacs.com>
2013-03-04 01:53:26 +0000

    Added date to trades.

--- datm/models/trade.py
+++ datm/models/trade.py
@@ -8,15 +8,17 @@ class Trade(base.Base):
     __tablename__ = 'trades'
 
     id = Column(Integer, primary_key=True)
+    date = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
     purchase = Column(Boolean, nullable=False)
 
     user_id = Column(Integer, ForeignKey("users.id"))
     artist_id = Column(String, ForeignKey("artists.mbid"))
 
-    def __init__(self, user, artist, price, purchase):
+    def __init__(self, user, artist, price, purchase, date):
         """Create new ``Trade`` for ``user``, of ``artist``, at ``price``."""
         self.user = user
         self.artist = artist
         self.price = price
         self.purchase = purchase
+        self.date = date
