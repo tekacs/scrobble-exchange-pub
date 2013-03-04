f99637b1c5021ab998f1a108ae5d4ad78aa6938e
Neil <ns532@cam.ac.uk>
2013-03-04 04:57:11 +0000

    Show welcome template for new visitors

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -39,7 +39,7 @@ def home(request):
 
         return render_to_response('index.html', {'user_data': user_data}, context_instance=RequestContext(request))
     else:
-        return render_to_response('index.html', {}, context_instance=RequestContext(request))
+        return render_to_response('welcome.html', {}, context_instance=RequestContext(request))
 
 
 # @contextmanager
